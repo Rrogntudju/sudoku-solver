1ff7cc9464419ca9a177ffb53c7192eb3f6e78fd
Rrogntudju <mario.prince@outlook.com>
2017-11-04 12:52:15 -0400

    Strip.All

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
-from setuptools_rust import Binding, RustExtension
+from setuptools_rust import Binding, RustExtension, Strip
 
 setup(name='sudoku-solver',
       version='1.0',
       rust_extensions=[RustExtension('sudoku_solver.libsudokusolver',
-                                     'Cargo.toml', binding=Binding.PyO3)],
+                                     'Cargo.toml', binding=Binding.PyO3,
+                                     strip=Strip.All)],
       packages=['sudoku_solver'],
       # rust extensions are not zip safe, just like C-extensions.
       zip_safe=False
